1e1358c8adf4b875871e63ba7219ada85b0d1557
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-04-13 22:14:15 -0400

    test db and health setups

---
+++ tests/health/test_setup.py
@@ -0,0 +1,13 @@
+import pytest
+import mock
+from tests.cog_test_ext import assert_cog_added
+from discord.ext.commands import Bot
+from duckbot.health import setup as extension_setup, HealthCheck
+
+
+@pytest.mark.asyncio
+async def test_setup():
+    bot = mock.Mock(wraps=Bot(command_prefix="."))
+    extension_setup(bot)
+    await bot.close()
+    assert_cog_added(bot, HealthCheck)
