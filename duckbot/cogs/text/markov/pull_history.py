dbd4b83021ed9e2bde50910434fdef3a05f4392b
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-06-05 17:09:16 -0400

    wip markovinit

--- duckbot/cogs/text/markov/pull_history.py
+++ duckbot/cogs/text/markov/pull_history.py
@@ -18,8 +18,10 @@ class PullHistory(commands.Cog):
     async def markov_init_command(self, context, user: Member):
         channels = [c for c in self.bot.get_all_channels() if c.type == ChannelType.text]
         for channel in channels:
-            async for message in channel.history().filter(lambda msg: msg.author == user):
-                print(message)
+            messages, stilL_more = await self.fetch_batch(channel, user)
+            await self.store_batch(messages, user)
+            # async for message in channel.history().filter(lambda msg: msg.author == user):
+            #     print(message)
 
     async def fetch_batch(self, channel: TextChannel, user: Member, before: Optional[Message] = None):
         count = 0
@@ -31,6 +33,15 @@ class PullHistory(commands.Cog):
         return messages, count == 1000
 
     async def store_batch(self, messages: List[Message], user: Member):
-        pass
-        # words = word_tokenize()
-        # grams2 = ngrams(
+        async for n in self.ngram_range():
+            await self.store_ngram_batch(messages, user, n)
+
+    async def ngram_range(self):
+        for n in range(1, 4 + 1):
+            yield n
+
+    async def store_ngram_batch(self, messages: List[Message], user: Member, size: int):
+        for message in messages:
+            words = word_tokenize(message.clean_content)  # TODO remove markdown
+            grams = ngrams(words, size)
+            print([x for x in grams])
