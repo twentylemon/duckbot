b33a71296f1cf4d0d39ef4313832380f1ac33be7
twentylemon <taras.mychaskiw@gmail.com>
2021-10-17 14:57:33 -0400

    add numba optional to timezonefinder for speedup

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
             "discord.py[voice] @ git+https://github.com/Rapptz/discord.py",
             "beautifulsoup4",
             "pytz",
-            "timezonefinder>=5.2,<6",
+            "timezonefinder[numba]>=5.2,<6",
             "holidays>=0.11,<0.12",
             "pyowm>=3.2,<4",  # openweather
             "psycopg2",
