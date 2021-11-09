9dc86f735cb29f7a7263f3cf854a53b9de3ba86d
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2021-11-08 20:45:02 -0500

    add second schema option

--- duckbot/cogs/text/markov/ngrams.py
+++ duckbot/cogs/text/markov/ngrams.py
@@ -1,7 +1,40 @@
-from sqlalchemy import BigInteger, Column, Index, String
+from sqlalchemy import BigInteger, Column, Index, String, Integer, ForeignKeyConstraint
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+
+
+class Word(Base):
+    """Database mapping words to a numeric identifier."""
+
+    __tablename__ = "markov_words"
+
+    word_id = Column(BigInteger, primary_key=True, autoincrement=True, nullable=False)
+    word = Column(String, unique=True, nullable=False)
+
+
+class NgramWord(Base):
+    """Database specifying ngram members and relative ordering."""
+
+    __tablename__ = "markov_ngrams"
+
+    # in general, we'd want to answer "give me the next words in the chain where the previous words are blah (and ngrams are constrained to a user)"
+    # select ngram_id from ngrams where sequence_id=0 and word_id=0 and sequence_id=1 and word_id=1
+    ngram_id = Column(BigInteger, primary_key=True, autoincrement=True, nullable=False)
+    sequence_id = Column(Integer, primary_key=True, nullable=False)
+    word_id = Column(BigInteger, nullable=False)
+
+    __table_args__ = (ForeignKeyConstraint(("word_id",), "markov_words.word_id"),)
+
+
+class NgramUser(Base):
+    __tablename__ = "markov_ngram_users"
+
+    user_id = Column(BigInteger, primary_key=True, nullable=False)
+    ngram_id = Column(BigInteger, primary_key=True, nullable=False)
+    frequency = Column(BigInteger, nullable=False)
+
+    __table_args__ = (ForeignKeyConstraint(("ngram_id",), "markov_ngrams.ngram_id"),)
 
 
 class Ngram1(Base):
